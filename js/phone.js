78db8900433c461851d85fbbfca3f86fb42ba79f
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-28 13:07:19 +0600

    phone card details style finish

--- js/phone.js
+++ js/phone.js
@@ -9,7 +9,28 @@ const loadData = async () => {
 
 loadData();
 
+
 const phoneData = (phones) => {
-    
+    const cardDiv = document.getElementById('card-container');
+    phones.forEach( phoneCard => {
+        console.log(phoneCard);
+        const div = document.createElement('div');
+        div.classList = `card border-2 border-gray-200`
+        div.innerHTML = `
+        <div>
+            <figure class="px-10 pt-10">
+                <img src="${phoneCard.image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center space-y-4">
+                <h2 class="card-title text-2xl font-bold text-[#403f3f]">${phoneCard.phone_name}</h2>
+                <p>Tap the version number. Contact your carrier for more details.</p>
+                <div class="card-actions">
+                <button class="btn px-6 bg-[#0d6efd] btn text-lg font-bold text-white">Show Details</button>
+                </div>
+            </div>
+            </div>
+        `;
+        cardDiv.appendChild(div);
+    })
 }
 
